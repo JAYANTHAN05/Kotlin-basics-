b5711a494804e89ea9368d725408b5f921e1af1e
rmjay <rm.jayanthan05@gmail.com>
2023-12-01 11:11:44 +0530

    "kotlin basic"

---
+++ app/kotlin basics.kt
@@ -0,0 +1,59 @@
+// basics
+/* Creating var val
+   datatypes
+        Integer-byte,short,int,long
+        unSigned Int- UByte,UShort,UInt,ULong
+    print statements in Kotlin
+        print()
+        println()
+    Basic Operators in kotlin
+        Arithmetic Operators:
+            + (Addition)
+            - (Subtraction)
+            * (Multiplication)
+            / (Division)
+            % (Modulus)
+
+        Assignment Operators:
+            = (Assigns a value to a variable)
+            +=, -=, *=, /=, %= (Compound assignment operators, e.g., a += b is equivalent to a = a + b)
+       Comparison Operators:
+            == (Equality)
+            != (Inequality)
+            < (Less than)
+            > (Greater than)
+            <= (Less than or equal to)
+            >= (Greater than or equal to)
+        Logical Operators:
+            && (Logical AND)
+            || (Logical OR)
+            ! (Logical NOT)
+        Range Operator:
+            .. (Creates a range)
+
+
+        Bitwise Operators (Intermediate level):
+            shl (Signed shift left)
+            shr (Signed shift right)
+            ushr (Unsigned shift right)
+            and (Bitwise AND)
+            or (Bitwise OR)
+            xor (Bitwise XOR)
+            inv (Bitwise inversion)
+       Elvis Operator:
+            ?: (Elvis operator, used for null safety
+       Safe Call Operator:
+            ?. (Safe call operator, used for safe navigation when dealing with nulls)
+
+
+ */
+
+
+//code
+/*
+var a :Int= 10
+var b :Byte= 127
+var c :Short=37767
+var d :Long=9,223,372,036,854,775,807((263^63)-1) //63 is power
+*/
+
